64c3857f4809eab8a7468b332a26d34c0e7db40f
angelicagomezrs <angelicagomez@live.cl>
2015-06-21 18:31:18 -0300

    Mi parte
    holi

--- app/src/main/java/com/motivate/visual/IJuegoNoDiga.java
+++ app/src/main/java/com/motivate/visual/IJuegoNoDiga.java
@@ -1,5 +1,7 @@
 package com.motivate.visual;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -18,36 +20,79 @@ import com.motivate.nuevo.R;
 public class IJuegoNoDiga extends ActionBarActivity {
 
     Button comenzar;
-
     Button botonaceptar ;
     Button botonrechazar ;
-    private TextView mensaje_cuenta;
+    TextView mensaje_cuenta;
 
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nodigasinodigano);
-
-
         comenzar = (Button) findViewById(R.id.btn_comenzar);
         mensaje_cuenta=(TextView) findViewById(R.id.Titulo);
-
-        //aceptar();
-
-
         comenzar.setOnClickListener(new View.OnClickListener(){
             @Override
 
             public void onClick(View v) {
                 esperar();
+                comenzar.setEnabled(false);
             }
+        });
+
+        botonaceptar = (Button) findViewById(R.id.btn_aceptar);
+        botonaceptar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(IJuegoNoDiga.this);
+                builder.setMessage("Ganaste 10 puntos")
+                        .setTitle("Mensaje")
+                        .setCancelable(false)
+                        .setNeutralButton("Aceptar",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                        Intent nuevoform4 = new Intent(IJuegoNoDiga.this,IRuleta.class);
+                                        startActivity(nuevoform4);
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
 
 
+
+
+            }
         });
 
 
-   }
+        botonrechazar = (Button) findViewById(R.id.btn_rechazar);
+        botonrechazar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(IJuegoNoDiga.this);
+                builder.setMessage("Perdiste!")
+                        .setTitle("Mensaje")
+                        .setCancelable(false)
+                        .setNeutralButton("Aceptar",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                        Intent nuevoform4 = new Intent(IJuegoNoDiga.this,IRuleta.class);
+                                        startActivity(nuevoform4);
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+            }
+        });
+
+
+
+
+
+    }
 
 
     public void esperar(){
@@ -71,27 +116,29 @@ public class IJuegoNoDiga extends ActionBarActivity {
 
     }
 
+
     public void aceptar(){
         botonaceptar = (Button) findViewById(R.id.btn_aceptar);
         botonaceptar.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View view) {
-                        Intent nuevoformA = new Intent(IJuegoNoDiga.this,IRuleta.class);
-                        startActivity(nuevoformA);
+            public void onClick(View view) {
+                Intent nuevoformA = new Intent(IJuegoNoDiga.this,IRuleta.class);
+                startActivity(nuevoformA);
 
-                }
+            }
         });
     }
     public void rechazar(){
 
         botonrechazar = (Button) findViewById(R.id.btn_rechazar);
         botonrechazar.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View view) {
-                        Intent nuevoformR = new Intent(IJuegoNoDiga.this,IRuleta.class);
-                        startActivity(nuevoformR);
+            public void onClick(View view) {
+                Intent nuevoformR = new Intent(IJuegoNoDiga.this,IRuleta.class);
+                startActivity(nuevoformR);
 
-                }
+            }
         });
 
     }
+
 
 }
